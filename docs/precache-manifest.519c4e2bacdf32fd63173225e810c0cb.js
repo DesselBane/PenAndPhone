c76b31dc2109cff48c04e604ae99064fa3cef9d2
Jan Morawietz <jan.morawietz@paessler.com>
2020-09-16 11:17:26 +0200

    add Docs

---
+++ docs/precache-manifest.519c4e2bacdf32fd63173225e810c0cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    'revision': '5c85db7993e2976468bb',
+    'url': '/css/app.d10f083b.css',
+  },
+  {
+    'revision': '82b9c7a5a3f405032b1db71a25f67021',
+    'url': '/img/logo.82b9c7a5.png',
+  },
+  {
+    'revision': 'f3d6d1c3ea404bfc128452f99dae7e0e',
+    'url': '/index.html',
+  },
+  {
+    'revision': '6a03d2b00d583f6a60eb',
+    'url': '/js/about.c787ca3d.js',
+  },
+  {
+    'revision': '5c85db7993e2976468bb',
+    'url': '/js/app.b62358cf.js',
+  },
+  {
+    'revision': '90e4829ece2a04347978',
+    'url': '/js/chunk-vendors.d6c7fdf4.js',
+  },
+  {
+    'revision': 'aa17d8366d43d5938b5e44da86bac8bc',
+    'url': '/manifest.json',
+  },
+  {
+    'revision': 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    'url': '/robots.txt',
+  },
+])
