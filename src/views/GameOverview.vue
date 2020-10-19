a56c89fe32c9a0290d7410169aaba0991aff423d
Jan Morawietz <jan.morawietz@paessler.com>
2020-10-19 19:45:59 +0200

    add edit/delete characters

--- src/views/GameOverview.vue
+++ src/views/GameOverview.vue
@@ -1,7 +1,21 @@
 <template>
   <EpicHeading class="mb-7">Deine Helden</EpicHeading>
-  <EpicCard class="mb-8" v-for="{ id, name } in chars" :key="id">
-    <EpicHeading as="h3" class="mb-4">{{ name }}</EpicHeading>
+  <EpicCard class="mb-8" v-for="char in chars" :key="char.id">
+    <EpicHeading as="h3" class="mb-4">{{ char.name }}</EpicHeading>
+    <EpicButton
+      class="gr-3 gc-1"
+      as="router-link"
+      :to="`/game/${game.id}/char/${char.id}/edit`"
+      >Editieren
+    </EpicButton>
+    <EpicButton
+      class="gr-3 gc-2"
+      as="button"
+      @click="() => openDeleteModal(char)"
+      icon="delete"
+      icononly
+      >Löschen
+    </EpicButton>
   </EpicCard>
   <EpicButton primary @click="openCreateModal" icon="add">
     Held erstellen
@@ -12,6 +26,19 @@
       <EpicInput v-model="newChar.name" label="Spielname" class="mb-6" />
       <EpicButton primary fullwidth>Held erstellen</EpicButton>
     </form>
+  </EpicModal>
+
+  <EpicModal v-model:is-open="confirmDeleteModalIsOpen">
+    <EpicHeading as="h2" class="mb-6"
+      >Soll dieser Held wirklich gelöscht werden?
+    </EpicHeading>
+    <EpicHeading as="h3" class="mb-4 gr-1 gcs-1-3 "
+      >Character: {{ deleteCharRef.name }}
+    </EpicHeading>
+    <EpicButton fullwidth @click="cancleDeleteChar" class="mb-2"
+      >Abbrechen
+    </EpicButton>
+    <EpicButton primary fullwidth @click="deleteChar">Löschen</EpicButton>
   </EpicModal>
 </template>
 
@@ -40,12 +67,12 @@ export default defineComponent({
   setup() {
     const route = useRoute()
     const game = computed(() => {
-      return storeInstance.games.value.find(
-        (x) => x.id === Number(route.params.gameId)
-      )
+      return storeInstance.getGameById(Number(route.params.gameId))
     })
     const chars = game.value.characters
     const newChar = ref(new Character())
+    const deleteCharRef = ref(new Character())
+    const confirmDeleteModalIsOpen = ref(false)
 
     const createModalIsOpen = ref(false)
 
@@ -59,12 +86,34 @@ export default defineComponent({
       newChar.value = new Character()
     }
 
+    function deleteChar() {
+      game.value.removeCharacter(deleteCharRef.value)
+      deleteCharRef.value = new Character()
+      confirmDeleteModalIsOpen.value = false
+    }
+
+    function openDeleteModal(char: Character) {
+      deleteCharRef.value = char
+      confirmDeleteModalIsOpen.value = true
+    }
+
+    function cancleDeleteChar() {
+      deleteCharRef.value = new Character()
+      confirmDeleteModalIsOpen.value = false
+    }
+
     return {
+      game,
       chars,
       newChar,
       createChar,
       openCreateModal,
       createModalIsOpen,
+      deleteChar,
+      openDeleteModal,
+      cancleDeleteChar,
+      deleteCharRef,
+      confirmDeleteModalIsOpen,
     }
   },
 })
