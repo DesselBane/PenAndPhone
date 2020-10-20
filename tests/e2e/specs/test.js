c704a3a31e6f9dd786b66d11dec568c8c760c910
Dessel Bane <dessel.bane@gmx.de>
2020-10-20 18:03:52 +0200

    setup Cypress CI

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,6 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('App', () => {
+  it('App frame exists', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.get('[data-ta="app-main"]').should('exist')
   })
 })
