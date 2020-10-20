bd592cd10d29afaaca84e5607ac753be35740fe8
Dessel Bane <dessel.bane@gmx.de>
2020-10-20 16:50:11 +0200

    initial docs setup

---
+++ docs/precache-manifest.29328b49c2e8bf50a3fef2e13c74cad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342a66f3e9e63a9dfddf",
+    "url": "css/app.a5be201e.css"
+  },
+  {
+    "revision": "56906712bac5e7b4aa557bf079ba53d2",
+    "url": "index.html"
+  },
+  {
+    "revision": "342a66f3e9e63a9dfddf",
+    "url": "js/app.68551f70.js"
+  },
+  {
+    "revision": "98e86053f06f41d4f5e6",
+    "url": "js/chunk-vendors.291f1f98.js"
+  },
+  {
+    "revision": "aa17d8366d43d5938b5e44da86bac8bc",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
