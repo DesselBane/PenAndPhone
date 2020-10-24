169f360698d140d870699ff8ee74d2e5c49754d7
JB <jb@visualjerk.de>
2020-10-24 17:28:42 +0200

    refactor: input behavior

--- src/components/EpicAttributeInput.vue
+++ src/components/EpicAttributeInput.vue
@@ -1,54 +1,45 @@
 <template>
-  <label class="epic-attribute-input-wrapper">
-    {{ label }}
+  <EpicInputItem :label="label">
     <div class="epic-attribute-input-controls">
       <input
-        ref="inputEl"
+        v-bind="inputAttrs"
         class="epic-attribute-input"
         :value="modelValue"
-        @input="updateValue($event.target.value)"
         readonly
       />
-      <EpicButton icon="arrow-down" icononly />
-      <EpicButton icon="arrow-up" icononly />
+      <EpicButton
+        icon="arrow-down"
+        icononly
+        @click="updateValue(modelValue - 1)"
+      />
+      <EpicButton
+        icon="arrow-up"
+        icononly
+        @click="updateValue(modelValue + 1)"
+      />
     </div>
-  </label>
+  </EpicInputItem>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, Ref } from 'vue'
+import { defineComponent } from 'vue'
+import { inputProps, useInput } from './shared/input'
 import EpicButton from '@components/EpicButton'
+import EpicInputItem from '@components/EpicInputItem'
 
 export default defineComponent({
   name: 'EpicAttributeInput',
   components: {
     EpicButton,
+    EpicInputItem,
   },
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
-    },
-    label: {
-      type: String,
-      default: '',
-    },
-  },
-  setup(_, { emit }) {
-    const inputEl: Ref<HTMLElement | null> = ref(null)
-
-    function updateValue(value: string) {
-      emit('update:modelValue', value)
-    }
-
-    function focus() {
-      if (inputEl.value != null) {
-        inputEl.value.focus()
-      }
-    }
+  props: inputProps,
+  setup(_, ctx) {
+    const { inputEl, inputAttrs, updateValue, focus } = useInput(ctx)
 
     return {
       inputEl,
+      inputAttrs,
       updateValue,
       focus,
     }
@@ -57,16 +48,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-@import 'src/styles/shared.scss';
-
-.epic-attribute-input-wrapper {
-  display: grid;
-  gap: 0.5rem;
-  font-weight: 600;
-  color: rgba($color-text, 0.65);
-  text-align: left;
-  letter-spacing: 0.05em;
-}
+@import './shared/input.scss';
 
 .epic-attribute-input-controls {
   display: grid;
@@ -74,30 +56,7 @@ export default defineComponent({
   gap: 0.5rem;
 }
 
-$input-base-shadow: 0 0 2px shadow(0.3), 0 4px 12px -4px shadow(0.2),
-  inset 0 0 0 1px shadow(0.3), inset 0 0 0 2px light(0.9),
-  inset 0 0 5px 1px shadow(0.15);
-
 .epic-attribute-input {
-  all: initial;
-  min-width: 0;
-  padding: 0.7rem 1.2rem;
-  font-size: 1.1rem;
-  color: $color-text;
-  letter-spacing: 0.05em;
-  border-radius: 0.1rem;
-  box-shadow: $input-base-shadow;
-  transition: box-shadow 0.05s ease-out;
-
-  @include bg-paper($color-body);
-
-  &:focus {
-    box-shadow: $input-base-shadow, 0 10px 26px -10px rgba($color-primary, 0.5),
-      0 0 5px rgba($color-primary, 0.3);
-  }
-
-  &::placeholder {
-    color: rgba($color-text, 0.35);
-  }
+  @include input;
 }
 </style>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,21 +1,17 @@
 <template>
   <div>
-    <EpicHeading class="mb-7">Brandur Boraldin</EpicHeading>
-    <EpicCard class="mb-8" v-for="{ id, name } in games" :key="id">
-      <EpicHeading as="h3" class="mb-6">{{ name }}</EpicHeading>
-      <EpicAttributeInput label="Stärke" :modelValue="2" class="mb-6" />
-      <EpicAttributeInput label="Ausdauer" :modelValue="3" class="mb-6" />
-      <EpicAttributeInput label="Willenskraft" :modelValue="1" class="mb-6" />
+    <EpicHeading class="mb-7">{{ name }}</EpicHeading>
+    <EpicCard class="mb-8">
+      <EpicInput label="Name" v-model="name" class="mb-6" />
+      <EpicAttributeInput label="Stärke" v-model="strenght" class="mb-6" />
+      <EpicAttributeInput label="Ausdauer" v-model="stamina" class="mb-6" />
+      <EpicAttributeInput
+        label="Willenskraft"
+        v-model="willpower"
+        class="mb-6"
+      />
       <EpicButton as="router-link" :to="`/game/${id}`">Speichern</EpicButton>
     </EpicCard>
-    <EpicButton primary @click="openCreateModal">+ Spiel erstellen</EpicButton>
-    <EpicModal v-model:is-open="createModalIsOpen">
-      <EpicHeading as="h2" class="mb-6">Neues Spiel erstellen</EpicHeading>
-      <form @submit.prevent="createGame">
-        <EpicInput modelValue="" label="Spielname" class="mb-6" />
-        <EpicButton primary fullwidth>Spiel erstellen</EpicButton>
-      </form>
-    </EpicModal>
   </div>
 </template>
 
@@ -24,7 +20,6 @@ import { defineComponent, ref } from 'vue'
 import EpicCard from '@components/EpicCard.vue'
 import EpicHeading from '@components/EpicHeading.vue'
 import EpicButton from '@components/EpicButton.vue'
-import EpicModal from '@components/EpicModal.vue'
 import EpicInput from '@components/EpicInput.vue'
 import EpicAttributeInput from '@components/EpicAttributeInput.vue'
 
@@ -34,37 +29,20 @@ export default defineComponent({
     EpicCard,
     EpicHeading,
     EpicButton,
-    EpicModal,
     EpicInput,
     EpicAttributeInput,
   },
   setup() {
-    const games = ref([
-      {
-        id: '1',
-        name: 'Attribute',
-      },
-      {
-        id: '2',
-        name: 'Dragonland Witch Hunt',
-      },
-    ])
-
-    const createModalIsOpen = ref(false)
-    function openCreateModal() {
-      createModalIsOpen.value = true
-    }
-
-    function createGame() {
-      // Create da epic game HEEEERRREEE
-      createModalIsOpen.value = false
-    }
+    const name = ref('Brandur Boraldin')
+    const strenght = ref(2)
+    const stamina = ref(3)
+    const willpower = ref(1)
 
     return {
-      games,
-      createModalIsOpen,
-      openCreateModal,
-      createGame,
+      name,
+      strenght,
+      stamina,
+      willpower,
     }
   },
 })
