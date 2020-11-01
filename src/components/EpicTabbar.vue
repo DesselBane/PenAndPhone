5f5c23df6d1d9679d45700b1c317cbd1fecbb800
Dessel Bane <dessel.bane@gmx.de>
2020-11-01 14:11:36 +0100

    add Tabbar before element

--- src/components/EpicTabbar.vue
+++ src/components/EpicTabbar.vue
@@ -16,10 +16,22 @@ export default defineComponent({
 @import 'src/styles/shared.scss';
 
 .epic-tabbar {
+  position: relative;
   display: flex;
   justify-content: space-evenly;
   overflow: hidden;
   box-shadow: 0 0 2px shadow(0.3), 0 -4px 20px 0 shadow(0.3);
+
+  ::before {
+    position: absolute;
+    top: 0;
+    right: -1rem;
+    bottom: -1rem;
+    left: -1rem;
+    content: '';
+    pointer-events: none;
+    box-shadow: inset 0 0 1px 2px light(0.9), inset 0 0 4px 1px shadow(0.4);
+  }
 
   @include bg-paper('white');
 }
